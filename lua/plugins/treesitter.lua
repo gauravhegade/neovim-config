84a05aadfe66a253ca7798bd423180d82d90e619
Gaurav Hegade <neph005@protonmail.com>
2024-03-17 14:12:58 +0530

    Add copilot and bufferline support

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,12 +1,12 @@
-return { 
-    "nvim-treesitter/nvim-treesitter",
+return {
+   "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate" ,
     config = function()
         local config = require("nvim-treesitter.configs")
         config.setup({
             ensure_installed = {"lua", "javascript", "python", "html", "go", "css", "cpp", "c"},
             highlight = { enable = true },
-            indent = { enable = true },  
+            indent = { enable = true },
         })
     end
 }
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,5 +4,14 @@ vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.cmd("set relativenumber")
 vim.cmd("set wrap linebreak textwidth=80")
+
 vim.g.mapleader = " "
+
+vim.opt.termguicolors = true
+
+vim.keymap.set("n", "<S-l>", ":bnext<CR>", {})
+vim.keymap.set("n", "<S-h>", ":bprevious<CR>", {})
+vim.keymap.set("n", "<A-j>", "<Esc>:m .+1<CR>==", {})
+vim.keymap.set("n", "<A-k>", "<Esc>:m .-2<CR>==", {})
+vim.keymap.set("n", "<leader>x", "<cmd> bp|sp|bn|bd! <CR>")
 
